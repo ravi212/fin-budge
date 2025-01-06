5f1acf1e18d7bf701ec19daf5bc55691b5b71ff1
ravi212 <raviraina212@gmail.com>
2025-01-06 20:57:44 +0530

    completed landing page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,118 @@
 import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  featuresData,
+  howItWorksData,
+  statsData,
+  testimonialsData,
+} from "@/lib/landing_data";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="mt-40">
       <HeroSection />
+
+      <section className="py-20 bg-blue-50">
+        <div className="container mx-auto px-4">
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
+            {statsData.map((item, index) => (
+              <div key={index} className="text-center">
+                <div className="text-4xl font-bold mb-2 text-blue-600">
+                  {item?.value}
+                </div>
+                <div className="text-gray-600">{item?.label}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20 ">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-12 text-center">
+            Evenything you need to manage your finances
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {featuresData.map((feature, index) => (
+              <Card key={index} className="p-6">
+                <CardContent className="space-y-4 pt-4">
+                  {feature.icon}
+                  <h3 className="font-semibold text-xl">{feature.title}</h3>
+                  <p className="text-gray-600">{feature.description}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20 bg-blue-50">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-16 text-center">{`How it work's`}</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {howItWorksData.map((step, index) => (
+              <div key={index} className="text-center">
+                <div className="w-16 h-16 bg-blue-100 rounded-full flex items-center mb-6 mx-auto justify-center">
+                  {step.icon}
+                </div>
+                <h3 className="text-xl font-semibold mb-4">{step.title}</h3>
+                <p className="text-gray-600">{step.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20 ">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-12 text-center">
+            What Our Users Say
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {testimonialsData.map((testimonial, index) => (
+              <Card key={index} className="p-6">
+                <CardContent className="pt-4">
+                  <div className="flex items-center space-x-4 mb-4">
+                    <Image
+                      src={testimonial.image}
+                      alt={testimonial.name}
+                      width={40}
+                      height={40}
+                      className="rounded-full"
+                    />
+                  <div className="ml-4">
+                    <div className="font-semibold">{testimonial.name}</div>
+                    <div className="text-sm text-gray-600">{testimonial.role}</div>
+                  </div>
+                  </div>
+                  <p className="text-gray-600">{testimonial.quote}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20 bg-blue-600">
+        <div className="container mx-auto px-4 text-center">
+          <h2 className="text-3xl font-bold mb-4 text-center text-white">
+            Ready to Take Control of Your Finances?
+          </h2>
+          <p className="text-blue-100 mb-8 max-w-2xl mx-auto">
+            Join thousands of users who are managing their 
+            finances
+            smarter with FinBudge
+          </p>
+          <Link href="/dashboard">
+            <Button size="lg" className="bg-white text-blue-600 hover:bg-blue-50 animate-bounce">
+              Start Free Trial
+            </Button>
+          </Link>
+        </div>
+      </section>
     </div>
   );
 }
--- components/hero.jsx
+++ components/hero.jsx
@@ -1,10 +1,63 @@
-"use client"
-import React from 'react'
+"use client";
+import React, { useEffect, useRef } from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 const HeroSection = () => {
-  return (
-    <div>HeroSection</div>
-  )
-}
 
-export default HeroSection
+  const imageRef = useRef();
+
+  useEffect(() => {
+    const imageElement = imageRef?.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition>scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => window.removeEventListener("scroll", handleScroll)
+
+  }, [])
+
+  return (
+    <div className="pb-20 px-4">
+      <div className="container mx-auto text-center">
+        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
+          Manage Tour Finances <br /> with Intelligence
+        </h1>
+        <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">AI Powered financial management platform</p>
+        <div>
+          <Link href={"/dashboard"}>
+            <Button size="lg" className="px-8">
+              Get Started
+            </Button>
+          </Link>
+        </div>
+        <div className="hero-image-wrapper">
+        <div ref={imageRef} className="hero-image">
+        <Image
+            src={"/banner.webp"}
+            width={1280}
+            height={720}
+            alt="Dashboard Preview"
+            className="object-contain rounded-lg mx-auto shadow-2xl border"
+            priority
+          />
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
